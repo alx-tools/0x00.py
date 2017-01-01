5ccf7960cf1eaa83d6fb655ecc04fa60ea124e42
Julien Barbier <write0@gmail.com>
2017-01-01 13:46:53 -0800

    Update 8-concat_edges.py

--- 8-concat_edges.py
+++ 8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-# YOUR CODE HERE
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(str)
