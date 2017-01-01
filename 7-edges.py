8d0e7f477a781b17f73e418bb494bf1a09e8c148
Julien Barbier <write0@gmail.com>
2017-01-01 13:41:20 -0800

    Update 7-edges.py

--- 7-edges.py
+++ 7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE HERE
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
