a5c8b6018fd728a337ab6a21dfe921f6da6c981f
Guillaume SALVA <ghinzu.iphone@gmail.com>
2017-01-01 13:21:36 -0800

    Create 8-concat_edges.py

---
+++ 8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+# YOUR CODE HERE
+print(str)
