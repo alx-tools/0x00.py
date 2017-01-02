253d812637e27adbb5d59a68f423509300bbbac0
PapaMuziko <ghinzu.iphone@gmail.com>
2017-01-02 08:06:02 -0800

    Fix PEP8

--- 8-concat_edges.py
+++ 8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(str)
